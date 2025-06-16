2585d724185dc56a53157d10018c8e3e4c97ae5e
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-06-16 20:33:15 +0530

    variables in braces

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,10 +3,10 @@
 PERSON1=dinesh 
 PERSON2=deepthi
 
-echo "$PERSON1: hey $PERSON2, how are you?"
+echo "${PERSON1}: hey ${PERSON2}, how are you?"
 
-echo "$PERSON2: hello $PERSON1, I am good"
+echo "${PERSON2}: hello ${PERSON1}, I am good"
 
-echo "$PERSON1: I love $PERSON2 so much"
+echo "${PERSON1}: I love ${PERSON2} so much"
 
-echo "$PERSON2: love you too $PERSON1"
+echo "${PERSON2}: love you too ${PERSON1}"
