5d1721bbb4cfbf236d0c6e1db9d144ef8422b642
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-06-18 10:45:18 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,10 +2,10 @@
 
 NUMBER=$1
 
-if [ $NUMBER -gt 100 ]
+if [ $NUMBER -lt 100 ]
 then 
-    echo "given number $NUMBER is graterthan 100" 
+    echo "given number $NUMBER is lessthan 100"
 else
-    echo "given number $NUMBER is not graterthan 100"
-fi 
-        
+    echo "given number $NUMBER is not lessthan 100"
+fi
+             
