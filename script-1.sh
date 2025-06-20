aeda80d220c3a3f6ef4d17410a29c381be71adfd
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-06-20 18:30:54 +0530

    script 1 and 2

---
+++ script-1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+GREETING="Hello, good morning" 
+echo "$GREETING" 
+echo "PID of script-1:$$"  # process ID of the current script: $$
+
+./script-2.sh #starting lo unna dot(.) represents current directory
