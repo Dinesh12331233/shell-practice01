9a262b29398b4466bcf20b79944eabdc95ca6a83
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-06-23 19:45:41 +0530

    set

--- 17-set.sh
+++ 17-set.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e #error vasthe automatic ga exit avutundhi
+       #shell lo error vasthe automatic ga exit avadhu. this is disadvantage in shell-scripting 
+       
 echo "Hi, good morning"
 echooooo "this will be the error"
 echo "hello, morning" 
