a5923198a8b3bc457bb51e8d4260dd3308efa589
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-06-24 17:28:45 +0530

    backup

--- 20-backup.sh
+++ 20-backup.sh
@@ -49,14 +49,14 @@ fi
 
 if [ ! -d $SOURCE_DIR ]
 then 
-    echo -e "$R $SOURCE_DIR does not exist. please check $N"
+    echo -e "$R source directory $SOURCE_DIR does not exist. please check $N"
     exit 1
 fi 
 
 
 if [ ! -d $DEST_DIR ]
 then 
-    echo -e "$R $DEST_DIR does not exist. please check $N"
+    echo -e "$R destination directory $DEST_DIR does not exist. please check $N"
     exit 1
 fi 
 
