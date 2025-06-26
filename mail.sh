63a2170e551c8d8462ff97c14a1a8a147b3e8d41
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-06-26 20:27:51 +0530

    mail

--- mail.sh
+++ mail.sh
@@ -8,7 +8,7 @@ FINAL_MSG=$(printf '%s\n' "$MSG" | sed -e 's/[]\/$*.^[]/\\&/g') #print chesi and
 TO_ADDRESS=$5 
 SUBJECT=$6 
 
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MSG/$MSG" )
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MSG/$FINAL_MSG" template.html)
 
 
 {
