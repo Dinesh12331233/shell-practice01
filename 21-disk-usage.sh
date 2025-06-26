129887eae50df00ad7d34dea0b9a6e652f116d96
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-06-26 19:21:00 +0530

    disk

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -2,13 +2,18 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 #In projects it will be 75% 
-
+MSG=""
 
 
 while IFS= read LINE 
 do 
     USAGE=$(echo $LINE | awk '{print $6F}' | cut -d "%" -f1) 
     LOCATION=$(echo $LINE | awk '{print $7F}') 
-    echo "$LOCATION: $USAGE%" 
+    #echo "$LOCATION: $USAGE%" 
+
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then 
+        MSG="HIGH Disk Usage on $LOCATION: $USAGE%" 
+    fi     
 
 done <<< $DISK_USAGE 
